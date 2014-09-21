732f472c459672ea5d66490c0ec9bb6331dda9b2
Justin Pando <justin.pando@gmail.com>
2014-09-20 18:59:43 -0700

    Added additional behaviors for visual effects and added pro image effects

---
+++ Assets/Scripts/Behaviors/KeepAwayBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepAwayBehavior : MonoBehaviour {
+	public GameObject objectToAvoid = null;
+	private Vector3 currentPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
